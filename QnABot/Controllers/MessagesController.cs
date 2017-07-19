25011f3d66001ecabce5ea3ad94e298030300a7a
Алексей Токарев <tokalig@mail.ru>
2017-07-19 14:10:51 +0300

    Updating libraries

--- QnABot/Controllers/MessagesController.cs
+++ QnABot/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.EnterpriseServices;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -14,7 +15,7 @@ namespace QnABot
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
-        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        public async Task<HttpResponseMessage> Post([FromBody]Microsoft.Bot.Connector.Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
             {
@@ -28,7 +29,7 @@ namespace QnABot
             return response;
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        private Microsoft.Bot.Connector.Activity HandleSystemMessage(Microsoft.Bot.Connector.Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
